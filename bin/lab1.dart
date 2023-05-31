fecbba907aaa93051281203c3953755280f04c10
Snare295 <vladneum@gmail.com>
2023-05-31 07:16:54 +0300

    Parse input, deleted unused files

--- bin/lab1.dart
+++ bin/lab1.dart
@@ -1,26 +1,28 @@
-import 'package:lab1/lab1.dart' as lab1;
 import 'dart:io';
-import 'dart:convert';
 
 main(){
   stdout.write("Write value for a:");
   String? a = stdin.readLineSync();
-  if (a == "") {
+  if (a == "" || a == null) {
     print("You given an empty value, abort program");
     return;
   }
 
   stdout.write("Write value for b:");
   String? b = stdin.readLineSync();
-  if (b == "") {
+  if (b == "" || b == null ) {
     print("You given an empty value, abort program");
     return;
   }
 
   stdout.write("Write value for c:");
   String? c = stdin.readLineSync();
-  if (c == "") {
+  if (c == "" || c == null ) {
     print("You given an empty value, abort program");
     return;
   }
+  
+  int an = int.parse(a);
+  int bn = int.parse(b);
+  int cn = int.parse(b);
 }
