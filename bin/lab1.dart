386ba9465a12bc4310ea42526e88938a39fb139c
Snare295 <vladneum@gmail.com>
2023-05-31 12:05:28 +0300

    Added square equation calc and output for it

--- bin/lab1.dart
+++ bin/lab1.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 main(){
   bool? ver;
@@ -49,11 +50,31 @@ main(){
       stdout.write("Error. Expected a valid real number, got $inp instead \n");
     }
   } while (!ver);
+  
+  //calculating and writing in terminal
+  double? x1,x2;
+  (x1, x2) = calculateSquareEq(a!, b!, c!);
+  
+  //output equation
+  stdout.write("Equation is: ($a) x^2 + ($b) x + ($c) = 0\n");
 
+  //output roots
+  if(x2 != null){
+    stdout.write("There are 2 roots\n");
+    stdout.write("x1 = $x1\n");
+    stdout.write("x2 = $x2\n");
+  } else
+  if(x1 != null){
+    stdout.write("There are 1 roots\n");
+    stdout.write("x1 = $x1\n");
+  } else {
+    stdout.write("There are 0 roots\n");
+  }
+  
 }
 
 bool isNumber(String? s){
-  if (s == null){
+  if (s == null || s == "NaN"){
     return false;
   }
 
@@ -64,3 +85,20 @@ bool isNumber(String? s){
   
   return true;
 }
+
+(double?, double?) calculateSquareEq(double a, double b, double c){
+  double? x1, x2;
+  double discr;
+
+  discr = pow(b,2) - (4*a*c);
+  print(discr); 
+  if(discr >= 0){
+    x1 = (-b + sqrt(discr)) / (2*a);
+  }
+  if(discr > 0) {
+    x2 = (-b - sqrt(discr)) / (2*a);
+  }
+  print(x1);
+  print(x2);
+  return (x1, x2);
+} 
