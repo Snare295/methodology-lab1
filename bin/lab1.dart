610dcf3d5a7eafb02aca16ed18f0163099935678
Snare295 <vladneum@gmail.com>
2023-06-01 00:46:18 +0300

    Formats main file

--- bin/lab1.dart
+++ bin/lab1.dart
@@ -1,182 +1,175 @@
-import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
-main(){
-  double a,b,c;
+main() {
+  double a, b, c;
   int? progMode;
 
-  //reading user input of program mode
+  //Reading user input of program mode
   stdout.write("Select program mode 1 or 2\n");
   stdout.write("1: Interactive mode, require input of a,b,c\n");
   stdout.write("2: File mode, read from file (for more help readme.md)\n");
   do {
     String? inp = stdin.readLineSync();
-    if(inp != null){
-    progMode = int.tryParse(inp);
+    if (inp != null) {
+      progMode = int.tryParse(inp);
     }
-    if(progMode == 1 || progMode == 2){
-    break;
+    if (progMode == 1 || progMode == 2) {
+      break;
     }
     stdout.write("Wrong input, select 1 or 2\n");
   } while (true);
 
   //switch for program mode
-  if(progMode == 1){
+  if (progMode == 1) {
     (a, b, c) = progModeInteractive();
-  } else 
-  if(progMode == 2){
+  } else if (progMode == 2) {
     (a, b, c) = progModeFile();
   } else {
-  throw "Unexpected behaviour: progMode isn't 1 or 2";
+    throw "Unexpected behaviour: progMode isn't 1 or 2";
   }
 
-  //calculating and writing in terminal
-  double? x1,x2;
+  //Calculating and writing in terminal
+  double? x1, x2;
   (x1, x2) = calculateSquareEq(a, b, c);
-  
-  //output equation
+
+  //Output equation
   stdout.write("Equation is: ($a) x^2 + ($b) x + ($c) = 0\n");
 
-  //output roots
-  if(x2 != null){
+  //Output roots
+  if (x2 != null) {
     stdout.write("There are 2 roots\n");
     stdout.write("x1 = $x1\n");
     stdout.write("x2 = $x2\n");
-  } else
-  if(x1 != null){
+  } else if (x1 != null) {
     stdout.write("There are 1 roots\n");
     stdout.write("x1 = $x1\n");
   } else {
     stdout.write("There are 0 roots\n");
   }
-  
 }
 
-bool isNumber(String? s){
-  if (s == null || s == "NaN"){
+bool isNumber(String? s) {
+  if (s == null || s == "NaN") {
     return false;
   }
 
   double? res = double.tryParse(s);
-  if (res == null){
+  if (res == null) {
     return false;
   }
-  
+
   return true;
 }
 
-(double?, double?) calculateSquareEq(double a,b,c){
+(double?, double?) calculateSquareEq(double a, b, c) {
   double? x1, x2;
   double discr;
 
-  discr = pow(b,2) - (4*a*c);
+  discr = pow(b, 2) - (4 * a * c);
 
-  if(discr >= 0){
-    x1 = (-b + sqrt(discr)) / (2*a);
+  if (discr >= 0) {
+    x1 = (-b + sqrt(discr)) / (2 * a);
   }
-  if(discr > 0) {
-    x2 = (-b - sqrt(discr)) / (2*a);
+  if (discr > 0) {
+    x2 = (-b - sqrt(discr)) / (2 * a);
   }
 
   return (x1, x2);
-} 
+}
 
-(double, double, double) progModeInteractive(){
-  double? a,b,c;
+(double, double, double) progModeInteractive() {
+  double? a, b, c;
   String? inp;
   bool? ver;
 
   //Input for var a
-  do{
+  do {
     ver = false;
     stdout.write("Write value for a:");
     inp = stdin.readLineSync();
 
     if (isNumber(inp) && inp != null) {
-      ver = true ;
+      ver = true;
       a = double.parse(inp);
-    }
-    else {
+    } else {
       stdout.write("Error. Expected a valid real number, got $inp instead \n");
     }
   } while (!ver);
 
   //Input for var b
-  do{
+  do {
     ver = false;
     stdout.write("Write value for b:");
     inp = stdin.readLineSync();
 
     if (isNumber(inp) && inp != null) {
-      ver = true ;
+      ver = true;
       b = double.parse(inp);
-    }
-    else {
+    } else {
       stdout.write("Error. Expected a valid real number, got $inp instead \n");
     }
   } while (!ver);
 
   //Input for var c
-  do{
+  do {
     ver = false;
     stdout.write("Write value for c:");
     inp = stdin.readLineSync();
 
     if (isNumber(inp) && inp != null) {
-      ver = true ;
+      ver = true;
       c = double.parse(inp);
-    }
-    else {
+    } else {
       stdout.write("Error. Expected a valid real number, got $inp instead \n");
     }
   } while (!ver);
-  
-return (a!, b!, c!);
+
+  return (a!, b!, c!);
 }
 
-(double, double, double) progModeFile(){
-  double? a,b,c;
+(double, double, double) progModeFile() {
+  double? a, b, c;
   String? inp;
   List<int> content;
 
   //Reading path of file
-  do{
+  do {
     stdout.write("Write path of file: ");
     inp = stdin.readLineSync();
-    
-    if(inp == null && inp == ""){
+
+    if (inp == null && inp == "") {
       stdout.write("Error. Empty input\n");
-    } else
-    if(File(inp!).existsSync() == false){
+    } else if (File(inp!).existsSync() == false) {
       stdout.write("Error. Invalid path\n");
-    } else
-    if(RegExp(".txt\$").hasMatch(inp) == false){
+    } else if (RegExp(".txt\$").hasMatch(inp) == false) {
       stdout.write("Error. Invalid file extension, need *.txt");
     } else {
       break;
     }
-  } while (true);  
-  
+  } while (true);
+
   content = File(inp).readAsBytesSync();
-  
+
   int first = content.indexOf(32);
   a = double.tryParse(String.fromCharCodes(content.take(first).toList()));
-  if(a == null){
+  if (a == null) {
     throw "Error. Invalid file formating, check README.md";
   }
   stdout.writeln("a is $a");
 
-  int second = content.indexOf(32, first+2);
-  b = double.tryParse(String.fromCharCodes(content.getRange(first, second).toList()));
-  if(b == null){
+  int second = content.indexOf(32, first + 2);
+  b = double.tryParse(
+      String.fromCharCodes(content.getRange(first, second).toList()));
+  if (b == null) {
     throw "Error. Invalid file formating, check README.md";
   }
   stdout.writeln("b is $b");
 
-  int third = content.indexOf(13, second+2);
-  c = double.tryParse(String.fromCharCodes(content.getRange(second, third).toList()));
-  if(c == null){
+  int third = content.indexOf(13, second + 2);
+  c = double.tryParse(
+      String.fromCharCodes(content.getRange(second, third).toList()));
+  if (c == null) {
     throw "Error. Invalid file formating, check README.md";
   }
   stdout.writeln("c is $c");
