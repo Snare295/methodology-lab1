07dc8954e5be514750fdf21362efafde872d0ac0
Snare295 <vladneum@gmail.com>
2023-05-31 08:24:21 +0300

    Reworked input, add checks

--- bin/lab1.dart
+++ bin/lab1.dart
@@ -1,28 +1,66 @@
 import 'dart:io';
 
 main(){
-  stdout.write("Write value for a:");
-  String? a = stdin.readLineSync();
-  if (a == "" || a == null) {
-    print("You given an empty value, abort program");
-    return;
+  bool? ver;
+  String? inp;
+  double? a,b,c;
+
+  //Input for var a
+  do{
+    ver = false;
+    stdout.write("Write value for a:");
+    inp = stdin.readLineSync();
+
+    if (isNumber(inp) && inp != null) {
+      ver = true ;
+      a = double.parse(inp);
+    }
+    else {
+      stdout.write("Error. Expected a valid real number, got $inp instead \n");
+    }
+  } while (!ver);
+
+  //Input for var b
+  do{
+    ver = false;
+    stdout.write("Write value for b:");
+    inp = stdin.readLineSync();
+
+    if (isNumber(inp) && inp != null) {
+      ver = true ;
+      b = double.parse(inp);
+    }
+    else {
+      stdout.write("Error. Expected a valid real number, got $inp instead \n");
+    }
+  } while (!ver);
+
+  //Input for var c
+  do{
+    ver = false;
+    stdout.write("Write value for c:");
+    inp = stdin.readLineSync();
+
+    if (isNumber(inp) && inp != null) {
+      ver = true ;
+      c = double.parse(inp);
+    }
+    else {
+      stdout.write("Error. Expected a valid real number, got $inp instead \n");
+    }
+  } while (!ver);
+
+}
+
+bool isNumber(String? s){
+  if (s == null){
+    return false;
   }
 
-  stdout.write("Write value for b:");
-  String? b = stdin.readLineSync();
-  if (b == "" || b == null ) {
-    print("You given an empty value, abort program");
-    return;
-  }
-
-  stdout.write("Write value for c:");
-  String? c = stdin.readLineSync();
-  if (c == "" || c == null ) {
-    print("You given an empty value, abort program");
-    return;
+  double? res = double.tryParse(s);
+  if (res == null){
+    return false;
   }
   
-  double an = double.parse(a);
-  double bn = double.parse(b);
-  double cn = double.parse(c);
+  return true;
 }
