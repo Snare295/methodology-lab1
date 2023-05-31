a128d9050b569fa8989a8b1dbd9c440867e23f7b
Snare295 <vladneum@gmail.com>
2023-05-31 12:57:19 +0300

    Add program modes, cut input into function

--- bin/lab1.dart
+++ bin/lab1.dart
@@ -2,10 +2,90 @@ import 'dart:io';
 import 'dart:math';
 
 main(){
-  bool? ver;
-  String? inp;
-  double? a,b,c;
+  double a,b,c;
+  int? progMode;
 
+  //reading user input of program mode
+  stdout.write("Select program mode 1 or 2\n");
+  stdout.write("1: Interactive mode, require input of a,b,c\n");
+  stdout.write("2: File mode, read from file (for more help readme.md)\n");
+  do {
+    String? inp = stdin.readLineSync();
+    if(inp != null){
+    progMode = int.tryParse(inp);
+    }
+    if(progMode == 1 || progMode == 2){
+    break;
+    }
+    stdout.write("Wrong input, select 1 or 2\n");
+  } while (true);
+
+  //switch for program mode
+  if(progMode == 1){
+    (a, b, c) = progModeInteractive();
+  } else 
+  if(progMode == 2){
+    (a, b, c) = progModeFile();
+  } else {
+  throw "Unexpected behaviour: progMode isn't 1 or 2";
+  }
+
+  //calculating and writing in terminal
+  double? x1,x2;
+  (x1, x2) = calculateSquareEq(a, b, c);
+  
+  //output equation
+  stdout.write("Equation is: ($a) x^2 + ($b) x + ($c) = 0\n");
+
+  //output roots
+  if(x2 != null){
+    stdout.write("There are 2 roots\n");
+    stdout.write("x1 = $x1\n");
+    stdout.write("x2 = $x2\n");
+  } else
+  if(x1 != null){
+    stdout.write("There are 1 roots\n");
+    stdout.write("x1 = $x1\n");
+  } else {
+    stdout.write("There are 0 roots\n");
+  }
+  
+}
+
+bool isNumber(String? s){
+  if (s == null || s == "NaN"){
+    return false;
+  }
+
+  double? res = double.tryParse(s);
+  if (res == null){
+    return false;
+  }
+  
+  return true;
+}
+
+(double?, double?) calculateSquareEq(double a,b,c){
+  double? x1, x2;
+  double discr;
+
+  discr = pow(b,2) - (4*a*c);
+  print(discr); 
+  if(discr >= 0){
+    x1 = (-b + sqrt(discr)) / (2*a);
+  }
+  if(discr > 0) {
+    x2 = (-b - sqrt(discr)) / (2*a);
+  }
+  print(x1);
+  print(x2);
+  return (x1, x2);
+} 
+
+(double, double, double) progModeInteractive(){
+  String? inp;
+  bool? ver;
+  double? a,b,c;
   //Input for var a
   do{
     ver = false;
@@ -51,54 +131,12 @@ main(){
     }
   } while (!ver);
   
-  //calculating and writing in terminal
-  double? x1,x2;
-  (x1, x2) = calculateSquareEq(a!, b!, c!);
-  
-  //output equation
-  stdout.write("Equation is: ($a) x^2 + ($b) x + ($c) = 0\n");
-
-  //output roots
-  if(x2 != null){
-    stdout.write("There are 2 roots\n");
-    stdout.write("x1 = $x1\n");
-    stdout.write("x2 = $x2\n");
-  } else
-  if(x1 != null){
-    stdout.write("There are 1 roots\n");
-    stdout.write("x1 = $x1\n");
-  } else {
-    stdout.write("There are 0 roots\n");
-  }
-  
+return (a!, b!, c!);
 }
 
-bool isNumber(String? s){
-  if (s == null || s == "NaN"){
-    return false;
-  }
+(double, double, double) progModeFile(){
+  double? a,b,c;
 
-  double? res = double.tryParse(s);
-  if (res == null){
-    return false;
-  }
-  
-  return true;
+
+  return (a!, b!, c!);
 }
-
-(double?, double?) calculateSquareEq(double a, double b, double c){
-  double? x1, x2;
-  double discr;
-
-  discr = pow(b,2) - (4*a*c);
-  print(discr); 
-  if(discr >= 0){
-    x1 = (-b + sqrt(discr)) / (2*a);
-  }
-  if(discr > 0) {
-    x2 = (-b - sqrt(discr)) / (2*a);
-  }
-  print(x1);
-  print(x2);
-  return (x1, x2);
-} 
